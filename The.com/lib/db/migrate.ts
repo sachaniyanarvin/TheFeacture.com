e4eb87b714e3246744f967e9f3bdb94ce71b64bd
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-07-02 15:27:02 +0530

    The.com/lib/db/migrate.ts: +4 -4

--- The.com/lib/db/migrate.ts
+++ The.com/lib/db/migrate.ts
@@ -4,7 +4,7 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
 config({
-  path: '.env.local',
+  path: '.env',
 });
 
 const runMigrate = async () => {
@@ -15,18 +15,18 @@ const runMigrate = async () => {
   const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
   const db = drizzle(connection);
 
-  console.log('⏳ Running migrations...');
+  console.log('Running migrations...');
 
   const start = Date.now();
   await migrate(db, { migrationsFolder: './lib/db/migrations' });
   const end = Date.now();
 
-  console.log('✅ Migrations completed in', end - start, 'ms');
+  console.log('Migrations completed in', end - start, 'ms');
   process.exit(0);
 };
 
 runMigrate().catch((err) => {
-  console.error('❌ Migration failed');
+  console.error('Migration failed');
   console.error(err);
   process.exit(1);
 });
