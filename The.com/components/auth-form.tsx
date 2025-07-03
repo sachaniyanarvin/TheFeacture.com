bd5d96a19e3821291bc582ed6e64f3b61d086b22
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-07-03 22:46:39 +0530

    make a eyes button for show password and hide password funcationality

--- The.com/components/auth-form.tsx
+++ The.com/components/auth-form.tsx
@@ -1,5 +1,6 @@
 import Form from 'next/form';
-
+import { useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 
@@ -14,6 +15,7 @@ export function AuthForm({
   children: React.ReactNode;
   defaultEmail?: string;
 }) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <Form action={action} className="flex flex-col gap-4 px-4 sm:px-16">
       <div className="flex flex-col gap-2">
@@ -45,13 +47,22 @@ export function AuthForm({
           Password
         </Label>
 
-        <Input
-          id="password"
-          name="password"
-          className="bg-muted text-md md:text-sm"
-          type="password"
-          required
-        />
+        <div className="relative">
+          <Input
+            id="password"
+            name="password"
+            className="bg-muted text-md md:text-sm pr-10"
+            type={showPassword ? 'text' : 'password'}
+            required
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute inset-y-0 right-0 flex items-center pr-3 text-zinc-400 dark:text-zinc-600"
+          >
+            {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
+          </button>
+        </div>
       </div>
 
       {children}
